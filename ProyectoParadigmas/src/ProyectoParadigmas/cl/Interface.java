761a0316e4a42fe0375ee2399e9a03f3b3a3c260
Andrés Villa <aivilla@alumnos.ubiobio.cl>
2014-05-12 16:13:04 -0400

    gfdhghj

--- ProyectoParadigmas/src/ProyectoParadigmas/cl/Interface.java
+++ ProyectoParadigmas/src/ProyectoParadigmas/cl/Interface.java
@@ -1,31 +1,98 @@
 package ProyectoParadigmas.cl;
 
 
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 public class Interface extends JFrame{
-	public static int ALTO = 800;
-	public static int ANCHO = 600;
-	public static String TITULO = "Redes de Ordenamiento";
+	
+	public static int ALTO = 600;
+	public static int ANCHO = 800;
+	public static String TITULO = "Redes de Ordenamiento.java";
 	
 	JTabbedPane pane = new JTabbedPane();
 	JPanel panelTutorial = new JPanel();
 	JPanel panelCrearRed = new JPanel();
+	//JPanel panelRed = new JPanel();
 	
 	JButton boton1 = new JButton("Entrada");
 	JButton boton2 = new JButton("Nodo");
 	JButton boton3 = new JButton("Red");
 	JButton boton4 = new JButton("Resultado");
 	
-	public interfaz(){
-		
-		
+	ButtonGroup migrupo = new ButtonGroup();
+	
+	
+	JButton boton5 = new JButton("Como Jugar");
+	JButton boton6 = new JButton("¿Que es un nodo? ");
+	JButton boton7 = new JButton("Mision del juego");
+	
+	//JButton boton8 = new JButton("-. Aqui se crea la Red .-");
+	
+	@SuppressWarnings("deprecation")
+	public Interface() {
+		super(TITULO);
+		setSize(ANCHO, ALTO);
+		addWindowListener(new WindowHandler());
+		ColocarComponentes();
+		setBackground(Color.pink);
+		show();
 	}
 	
+	void ColocarComponentes(){
+		
+		boton1.setBackground(Color.white);
+		boton2.setBackground(Color.white);
+		boton3.setBackground(Color.white);
+		boton4.setBackground(Color.white);
+		panelCrearRed.add(boton1);
+		panelCrearRed.add(boton2);
+		panelCrearRed.add(boton3);
+		panelCrearRed.add(boton4);
+		panelCrearRed.setBackground(Color.white);
+		panelCrearRed.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder()));	
+		
+		
+		
+		panelTutorial.setLayout(new GridLayout(0,1,0,100));
+		migrupo.add(boton5);
+		migrupo.add(boton6);
+		migrupo.add(boton7);
+		boton5.setBackground(Color.white);
+		boton6.setBackground(Color.white);
+		boton7.setBackground(Color.white);
+		panelTutorial.add(boton5);
+		panelTutorial.add(boton6);
+		panelTutorial.add(boton7);
+		panelTutorial.setBackground(Color.darkGray);
+		panelTutorial.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Elija un Tutorial: "));
+		
+		
+		pane.addTab("Tutorial", panelTutorial);
+		pane.addTab("Crear Red", panelCrearRed);
+		
+		this.getContentPane().add(pane);
+		}
 	
 	
-
+	public static void main(String[] args){
+		@SuppressWarnings("unused")
+		Interface ventana = new Interface();
+	}
+	
+	class WindowHandler extends WindowAdapter{
+		public void windowClosing(WindowEvent e){
+		System.exit(0);
+	}
+	}	
 }

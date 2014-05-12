159f5ff66c89901d05dddbc0acc1cc2d45d590d2
Andrés Villa <aivilla@alumnos.ubiobio.cl>
2014-05-12 14:37:31 -0400

    Avance 2

---
+++ ProyectoParadigmas/src/ProyectoParadigmas/cl/Interface.java
@@ -0,0 +1,31 @@
+package ProyectoParadigmas.cl;
+
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+public class Interface extends JFrame{
+	public static int ALTO = 800;
+	public static int ANCHO = 600;
+	public static String TITULO = "Redes de Ordenamiento";
+	
+	JTabbedPane pane = new JTabbedPane();
+	JPanel panelTutorial = new JPanel();
+	JPanel panelCrearRed = new JPanel();
+	
+	JButton boton1 = new JButton("Entrada");
+	JButton boton2 = new JButton("Nodo");
+	JButton boton3 = new JButton("Red");
+	JButton boton4 = new JButton("Resultado");
+	
+	public interfaz(){
+		
+		
+	}
+	
+	
+	
+
+}

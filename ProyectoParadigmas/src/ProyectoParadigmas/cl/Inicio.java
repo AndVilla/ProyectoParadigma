369b56d7b725d113276aaa4799b7ceb92b4694f4
7com <bl4ckt3ch@gmail.com>
2014-05-14 17:20:52 -0400

    Actualizado Inicio.java

--- ProyectoParadigmas/src/ProyectoParadigmas/cl/Inicio.java
+++ ProyectoParadigmas/src/ProyectoParadigmas/cl/Inicio.java
@@ -1,9 +1,12 @@
 package ProyectoParadigmas.cl;
 //menu inicio
 
-import java.awt.Component;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,13 +18,23 @@ public class Inicio {
 	public static final void menu(){
 		JFrame ventana = new JFrame();
 		ventana.setLayout(null);
+		ventana.setLocationRelativeTo(null);
 		JButton boton1 = new JButton("Tutorial");
 		JButton boton2 = new JButton("Crear");
 		JButton boton3 = new JButton("Abrir");
 		JButton boton4 = new JButton("Salir");
+		
+		boton4.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);	}});
+		
 		JLabel titulo = new JLabel("Redes de Ordenamiento");
 		ventana.setResizable(false);
 		ventana.setSize(190, 330);
+		Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
+		Dimension dimen = ventana.getSize();
+		ventana.setLocation((pantalla.width/2 - dimen.width/2), (pantalla.height/2 - dimen.height/2));
 		titulo.setBounds(24, 5, 200, 10);
 		ventana.add(titulo);
 		boton1.setBounds(45, 50, 100, 50);
@@ -33,6 +46,7 @@ public class Inicio {
 		boton4.setBounds(45, 230, 100, 50);
 		ventana.add(boton4);
 		ventana.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		ventana.getContentPane().setBackground(Color.white);
 		ventana.setVisible(true);
 	}
 }
